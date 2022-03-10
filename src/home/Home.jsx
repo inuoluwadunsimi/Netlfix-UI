0cc12d9eeedbf3302b3768a3084b3a1ce32c1f94
inuoluwadunsimi <danielolaoladeinde@gmail.com>
2022-03-10 11:16:29 +0100

    finished navbar

---
+++ src/home/Home.jsx
@@ -0,0 +1,40 @@
+import Navbar from "../components/navbar/Navbar"
+import "./home.scss"
+
+
+
+function Home() {
+  return (
+    <div className='home'>
+        
+        <Navbar/>
+        <img width="100%"src= "https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem reiciendis nostrum est voluptatibus velit officia recusandae. Necessitatibus blanditiis ratione officiis?
+
+        </div>
+  )
+}
+
+export default Home
